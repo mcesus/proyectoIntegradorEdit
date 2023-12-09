a4c816eed29fd30f3edaa9cd97dfe30fb073134d
mcesus <mceciliaalbornoz@gmail.com>
2023-12-09 14:29:39 -0300

    Estructura de controladores

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -2,7 +2,7 @@ module.exports = {
     home: (req, res) => res.render ('index'),
     contacto: (req,res)=> res.send('Esta es la vista de contacto'),
     faqs: (req, res)=> res.send ('Esta es la vista de preguntas frecuentes'),
-    item: (req, res) => res.render ('item')
+    about: (req, res) => res.send ('Esta es la vista de Nosotros')
 
 };
 
--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -1,13 +1,14 @@
 const express = require ('express');
 
 // const path = require ('path');
-const  {home, contacto, faqs} = require ('../controllers/main.controller');
+const controller = require ('../controllers/main.controller');
 
 const router = express.Router();
 
-router.get('/home', home);
-router.get('/contacto', contacto);
-router.get ('/faqs', faqs);
+router.get('/home', controller.home);
+router.get('/contacto', controller.contacto);
+router.get ('/faqs', controller.faqs);
+router.get ('/about', controller.about);
 
 
 
